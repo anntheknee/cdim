bd9748b4865199c8d33aebd9641f31a47af5580a
Anthony Lam <99anthonylam@gmail.com>
2020-05-16 22:07:55 -0400

    base code

--- script.sh
+++ script.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-dirs=$(find /home -type d | grep "$1$")
-i=1
-for OUTPUT in $dirs
-do
-    echo $i.")" $OUTPUT
-    i=$((i + 1))
-done
 
-read input
-set -- $dirs
+dirs=($(find / -iname $1 -type d 2>/dev/null))
+if [ ${#dirs[@]} -eq 1 ]
+then 
+    cd ${dirs[0]}
+else 
+    i=1
+    for dir in "${dirs[@]}"
+        do
+            if [ $i -gt 9 ]
+            then 
+                echo $i. $dir
+            else 
+                echo "$i.  $dir"
+            fi
+            ((i++))
+        done
+    read input
+    ((input--))
+    cd ${dirs[$input]}
+fi
 
-cd ${!input}
+
+
